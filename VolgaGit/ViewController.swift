ca08e40d4b3bf43ca58d6b71c12c48862fdd5fe4
John Greenwood <levask00@gmail.com>
2020-03-22 19:35:36 +0500

    Initial Commit

---
+++ VolgaGit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VolgaGit
+//
+//  Created by John on 22.03.2020.
+//  Copyright © 2020 Лебедев Лев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
